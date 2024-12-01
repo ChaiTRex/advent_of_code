9baeef4ede1a4e0db47e425b7e8e14b845a890cf
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-01 07:32:19 -0500

    Simplified 2024 day 1, part 2

--- 2024/rust/day01/src/main.rs
+++ 2024/rust/day01/src/main.rs
@@ -1,5 +1,3 @@
-use core::cmp::Ordering;
-
 static TIMING: bool = false;
 
 fn main() {
@@ -36,57 +34,25 @@ fn main() {
         println!("{part1}");
     }
 
-    fn catch_up(
-        values: &mut core::iter::Peekable<impl Iterator<Item = u32>>,
-        target_value: u32,
-    ) -> bool {
-        loop {
-            match values.peek().copied() {
-                None => return false,
-                Some(x) => {
-                    if x >= target_value {
-                        return true;
-                    } else {
-                        values.next();
-                    }
-                }
-            }
-        }
-    }
-
     let mut part2 = 0;
 
-    let mut left_values = left.into_iter().peekable();
-    let mut right_values = right.into_iter().peekable();
-    'outer: loop {
-        match (left_values.peek().copied(), right_values.peek().copied()) {
-            (None, _) => break 'outer,
-            (_, None) => break 'outer,
-            (Some(l), Some(r)) => match l.cmp(&r) {
-                Ordering::Equal => {
-                    let mut left_count = 0;
-                    while left_values.peek().copied() == Some(l) {
-                        left_values.next();
-                        left_count += 1;
-                    }
-                    let mut right_count = 0;
-                    while right_values.peek().copied() == Some(r) {
-                        right_values.next();
-                        right_count += 1;
-                    }
-                    part2 += l * left_count * right_count;
-                }
-                Ordering::Less => {
-                    if !catch_up(&mut left_values, r) {
-                        break 'outer;
-                    }
-                }
-                Ordering::Greater => {
-                    if !catch_up(&mut right_values, l) {
-                        break 'outer;
-                    }
-                }
-            },
+    let mut left = left.into_iter().peekable();
+    let mut right = right.into_iter().peekable();
+
+    while let Some(l) = left.next() {
+        let mut left_product = l;
+        while left.peek().copied() == Some(l) {
+            left.next();
+            left_product += l;
+        }
+
+        while right.peek().copied().map_or(false, |r| r < l) {
+            right.next();
+        }
+
+        while right.peek().copied().map_or(false, |r| r == l) {
+            right.next();
+            part2 += left_product;
         }
     }
 
