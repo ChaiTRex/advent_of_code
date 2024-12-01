f7869ace6b508ca932af6d10d38b653fb15aba06
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-01 00:15:16 -0500

    Initial solution to 2024 day 1

--- 2024/rust/day01/src/main.rs
+++ 2024/rust/day01/src/main.rs
@@ -1,4 +1,32 @@
 fn main() {
     static INPUT: &str = include_str!("../../../day01.txt");
-    println!("{INPUT}");
+
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = INPUT
+        .lines()
+        .map(|line| {
+            let mut iter = line.split_whitespace();
+            (
+                iter.next().unwrap().parse::<u32>().unwrap(),
+                iter.next().unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .unzip();
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let part1 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(left, right)| left.abs_diff(*right))
+        .sum::<u32>();
+
+    println!("{part1}");
+
+    let part2 = left
+        .iter()
+        .map(|x| x * right.iter().filter(|&y| x == y).count() as u32)
+        .sum::<u32>();
+
+    println!("{part2}");
 }
