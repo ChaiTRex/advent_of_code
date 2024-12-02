992c76af9a7a8a443814e55bcf76a72b45b4d422
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-02 00:27:04 -0500

    Initial solution to 2024 day 2

--- 2024/rust/day02/src/main.rs
+++ 2024/rust/day02/src/main.rs
@@ -1,4 +1,58 @@
 fn main() {
     static INPUT: &str = include_str!("../../../day02.txt");
-    println!("{INPUT}");
+
+    let inputs = INPUT
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut part1 = 0;
+    'outer: for input in &inputs {
+        if input[0] < input[1] {
+            for (a, b) in input.iter().zip(input.iter().skip(1)) {
+                if a >= b || !(1..=3).contains(&(b - a)) {
+                    continue 'outer;
+                }
+            }
+            part1 += 1;
+        } else if input[0] > input[1] {
+            for (a, b) in input.iter().zip(input.iter().skip(1)) {
+                if a <= b || !(1..=3).contains(&(a - b)) {
+                    continue 'outer;
+                }
+            }
+            part1 += 1;
+        }
+    }
+    println!("{part1}");
+
+    let mut part2 = 0;
+    'outer: for input in &inputs {
+        'remover: for i in 0..input.len() {
+            let mut input = input.clone();
+            input.remove(i);
+            if input[0] < input[1] {
+                for (a, b) in input.iter().zip(input.iter().skip(1)) {
+                    if a >= b || !(1..=3).contains(&(b - a)) {
+                        continue 'remover;
+                    }
+                }
+                part2 += 1;
+                continue 'outer;
+            } else if input[0] > input[1] {
+                for (a, b) in input.iter().zip(input.iter().skip(1)) {
+                    if a <= b || !(1..=3).contains(&(a - b)) {
+                        continue 'remover;
+                    }
+                }
+                part2 += 1;
+                continue 'outer;
+            }
+        }
+    }
+    println!("{part2}");
 }
