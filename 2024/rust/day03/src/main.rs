5bf84db41a8d3abb50c323207fdf94796a19594d
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-03 02:14:58 -0500

    Greatly sped up 2024 day 3 through insanity

--- 2024/rust/day03/src/main.rs
+++ 2024/rust/day03/src/main.rs
@@ -3,34 +3,100 @@ fn main() {
 
     let start = std::time::Instant::now();
 
-    let part1 = INPUT
-        .split("mul(")
-        .map(|thingy| {
-            let (a, rest) = thingy.split_once(',')?;
-            let (b, _) = rest.split_once(')')?;
-            let a = a.parse::<u32>().ok()?;
-            let b = b.parse::<u32>().ok()?;
-            Some(a * b)
-        })
-        .flatten()
-        .sum::<u32>();
+    let mut part1 = 0;
+    let mut part2 = 0;
+    let mut on = true;
+    let mut iter = INPUT.bytes();
 
-    let part2 = INPUT
-        .split("do()")
-        .map(|thingy| {
-            let (blah, _) = thingy.split_once("don't()").unwrap_or((thingy, ""));
-            blah.split("mul(")
-                .map(|thingy| {
-                    let (a, rest) = thingy.split_once(',')?;
-                    let (b, _) = rest.split_once(')')?;
-                    let a = a.parse::<u32>().ok()?;
-                    let b = b.parse::<u32>().ok()?;
-                    Some(a * b)
-                })
-                .flatten()
-                .sum::<u32>()
-        })
-        .sum::<u32>();
+    'outer: loop {
+        match iter.next() {
+            None => break,
+            Some(b'd') => match iter.next() {
+                None => break,
+                Some(b'o') => match iter.next() {
+                    None => break,
+                    Some(b'(') => match iter.next() {
+                        None => break,
+                        Some(b')') => on = true,
+                        _ => (),
+                    },
+                    Some(b'n') => match iter.next() {
+                        None => break,
+                        Some(b'\'') => match iter.next() {
+                            None => break,
+                            Some(b't') => match iter.next() {
+                                None => break,
+                                Some(b'(') => match iter.next() {
+                                    None => break,
+                                    Some(b')') => on = false,
+                                    _ => (),
+                                },
+                                _ => (),
+                            },
+                            _ => (),
+                        },
+                        _ => (),
+                    },
+                    _ => (),
+                },
+                _ => (),
+            },
+            Some(b'm') => match iter.next() {
+                None => break,
+                Some(b'u') => match iter.next() {
+                    None => break,
+                    Some(b'l') => match iter.next() {
+                        None => break,
+                        Some(b'(') => match iter.next() {
+                            None => break,
+                            Some(ch @ b'0'..=b'9') => {
+                                let mut a = (ch - b'0') as u32;
+                                loop {
+                                    match iter.next() {
+                                        None => break 'outer,
+                                        Some(ch @ b'0'..=b'9') => {
+                                            a = 10 * a + (ch - b'0') as u32;
+                                        }
+                                        Some(b',') => break,
+                                        _ => continue 'outer,
+                                    }
+                                }
+                                match iter.next() {
+                                    None => break,
+                                    Some(ch @ b'0'..=b'9') => {
+                                        let mut b = (ch - b'0') as u32;
+                                        loop {
+                                            match iter.next() {
+                                                None => break 'outer,
+                                                Some(ch @ b'0'..=b'9') => {
+                                                    b = 10 * b + (ch - b'0') as u32;
+                                                }
+                                                Some(b')') => {
+                                                    let product = a * b;
+                                                    part1 += product;
+                                                    if on {
+                                                        part2 += product;
+                                                    }
+                                                    break;
+                                                }
+                                                _ => continue 'outer,
+                                            }
+                                        }
+                                    }
+                                    _ => (),
+                                }
+                            }
+                            _ => (),
+                        },
+                        _ => (),
+                    },
+                    _ => (),
+                },
+                _ => (),
+            },
+            _ => (),
+        }
+    }
 
     let time = start.elapsed();
     println!("Part 1: {part1}\nPart 2: {part2}\nTime taken: {time:?}",);
