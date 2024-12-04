cce2237f761a8fe768575061cd4ab2bf589a33b1
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-04 04:44:03 -0500

    Further cleaned up code for 2024 day 4

--- 2024/rust/day04/src/main.rs
+++ 2024/rust/day04/src/main.rs
@@ -1,72 +1,71 @@
 fn main() {
-    static INPUT: &str = include_str!("../../../day04.txt");
+    static INPUT: &[u8] = include_bytes!("../../../day04.txt");
+    const WIDTH: usize = {
+        let mut i = 0;
+        while !INPUT[i].is_ascii_whitespace() {
+            i += 1;
+        }
+        i
+    };
+    const LINE_WIDTH: usize = {
+        let mut i = WIDTH;
+        while INPUT[i].is_ascii_whitespace() {
+            i += 1;
+        }
+        i
+    };
+    const HEIGHT: usize = INPUT.len() / LINE_WIDTH;
 
     let start = std::time::Instant::now();
 
     macro_rules! xmas_count_stage {
-        (
-            $input:ident,
-            $width:ident,
-            $height:ident,
-            $counter:ident,
-            $width_dec:literal,
-            $height_dec:literal,
-            $x_start:literal,
-            $x_step:literal,
-            $y_start:literal,
-            $y_step:literal
-        ) => {
-            for y in 0..$height - $height_dec {
-                for x in 0..$width - $width_dec {
-                    const X_0: usize = $x_start;
-                    const X_1: usize = X_0.wrapping_add_signed($x_step);
-                    const X_2: usize = X_1.wrapping_add_signed($x_step);
-                    const X_3: usize = X_2.wrapping_add_signed($x_step);
-                    const Y_0: usize = $y_start;
-                    const Y_1: usize = Y_0.wrapping_add_signed($y_step);
-                    const Y_2: usize = Y_1.wrapping_add_signed($y_step);
-                    const Y_3: usize = Y_2.wrapping_add_signed($y_step);
+        ($counter:ident, $input:ident, $line_width:ident, $width:ident, $height:ident :
+            { $($x_start_offset:literal, $x_end_offset:literal, $y_end_offset:literal,
+            $neighbor_offset:expr;)* }) => {
+            $(
+                for y in (0..$line_width * ($height - $y_end_offset)).step_by($line_width) {
+                    for x in $x_start_offset..$width - $x_end_offset {
+                        let i = x + y;
 
-                    if ($input[y + Y_0][x + X_0] == b'X'
-                        && $input[y + Y_1][x + X_1] == b'M'
-                        && $input[y + Y_2][x + X_2] == b'A'
-                        && $input[y + Y_3][x + X_3] == b'S')
-                        || ($input[y + Y_0][x + X_0] == b'S'
-                            && $input[y + Y_1][x + X_1] == b'A'
-                            && $input[y + Y_2][x + X_2] == b'M'
-                            && $input[y + Y_3][x + X_3] == b'X')
-                    {
-                        $counter += 1;
+                        const I_1: isize = $neighbor_offset;
+                        const I_2: isize = I_1 + $neighbor_offset;
+                        const I_3: isize = I_2 + $neighbor_offset;
+                        if ($input[i] == b'X'
+                            && $input[i.wrapping_add_signed(I_1)] == b'M'
+                            && $input[i.wrapping_add_signed(I_2)] == b'A'
+                            && $input[i.wrapping_add_signed(I_3)] == b'S')
+                            || ($input[i.wrapping_add_signed(I_3)] == b'X'
+                                && $input[i.wrapping_add_signed(I_2)] == b'M'
+                                && $input[i.wrapping_add_signed(I_1)] == b'A'
+                                && $input[i] == b'S')
+                        {
+                            $counter += 1;
+                        }
                     }
                 }
-            }
+            )*
         };
     }
 
-    let mut input = INPUT
-        .lines()
-        .map(|line| line.as_bytes())
-        .collect::<Vec<_>>();
-    let width = input[0].len();
-    let height = input.len();
-
     let mut part1 = 0;
-    xmas_count_stage!(input, width, height, part1, 3, 0, 0, 1, 0, 0);
-    xmas_count_stage!(input, width, height, part1, 0, 3, 0, 0, 0, 1);
-    xmas_count_stage!(input, width, height, part1, 3, 3, 0, 1, 0, 1);
-    xmas_count_stage!(input, width, height, part1, 3, 3, 3, -1, 0, 1);
+    xmas_count_stage!(part1, INPUT, LINE_WIDTH, WIDTH, HEIGHT: {
+        0, 3, 0, 1; // horizontal
+        0, 0, 3, LINE_WIDTH as isize; // vertical
+        0, 3, 3, LINE_WIDTH as isize + 1; // diagonal \
+        3, 0, 3, LINE_WIDTH as isize - 1; // diagonal /
+    });
 
     let mut part2 = 0;
-    for x in 0..width - 2 {
-        for y in 0..height - 2 {
-            if input[y + 1][x + 1] == b'A' {
-                if ((input[y][x] == b'M' && input[y + 2][x + 2] == b'S')
-                    || (input[y][x] == b'S' && input[y + 2][x + 2] == b'M'))
-                    && ((input[y + 2][x] == b'M' && input[y][x + 2] == b'S')
-                        || (input[y + 2][x] == b'S' && input[y][x + 2] == b'M'))
-                {
-                    part2 += 1;
-                }
+    for y in (LINE_WIDTH..LINE_WIDTH * (HEIGHT - 1)).step_by(LINE_WIDTH) {
+        for x in 1..WIDTH - 1 {
+            let i = x + y;
+            if INPUT[i] == b'A'
+                && ((INPUT[i - LINE_WIDTH - 1] == b'M' && INPUT[i + LINE_WIDTH + 1] == b'S')
+                    || (INPUT[i + LINE_WIDTH + 1] == b'M' && INPUT[i - LINE_WIDTH - 1] == b'S'))
+                && ((INPUT[i - LINE_WIDTH + 1] == b'M' && INPUT[i + LINE_WIDTH - 1] == b'S')
+                    || (INPUT[i + LINE_WIDTH - 1] == b'M' && INPUT[i - LINE_WIDTH + 1] == b'S'))
+            {
+                part2 += 1;
             }
         }
     }
