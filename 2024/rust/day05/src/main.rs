5e32a6dfaa6297445c4f7bb2275dc052ab0ab2f5
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-05 00:19:06 -0500

    Initial solution to 2024 day 5

--- 2024/rust/day05/src/main.rs
+++ 2024/rust/day05/src/main.rs
@@ -1,4 +1,74 @@
 fn main() {
     static INPUT: &str = include_str!("../../../day05.txt");
-    println!("{INPUT}");
+
+    let (rules, candidates) = INPUT.split_once("\n\n").unwrap();
+    let rules = rules
+        .lines()
+        .map(|rule| {
+            let (lower, higher) = rule.split_once('|').unwrap();
+            let lower = lower.parse::<u32>().unwrap();
+            let higher = higher.parse::<u32>().unwrap();
+            move |a: u32, b: u32| !(b == lower && a == higher)
+        })
+        .collect::<Vec<_>>();
+    let candidates = candidates
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let winners = candidates.iter().filter(|candidate| {
+        for i in 0..candidate.len() - 1 {
+            for j in i + 1..candidate.len() {
+                for rule in &rules {
+                    if !rule(candidate[i], candidate[j]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    });
+
+    let mut part1 = 0;
+    for winner in winners {
+        part1 += winner[winner.len() / 2];
+    }
+
+    println!("{part1}");
+
+    let mut part2 = 0;
+    let losers = candidates
+        .into_iter()
+        .filter(|candidate| {
+            for i in 0..candidate.len() - 1 {
+                for j in i + 1..candidate.len() {
+                    for rule in &rules {
+                        if !rule(candidate[i], candidate[j]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            false
+        })
+        .collect::<Vec<_>>();
+    for mut loser in losers {
+        loser.sort_by(|a, b| {
+            if a == b {
+                return core::cmp::Ordering::Equal;
+            }
+            for rule in &rules {
+                if !rule(*a, *b) {
+                    return core::cmp::Ordering::Greater;
+                }
+            }
+            return core::cmp::Ordering::Less;
+        });
+        part2 += loser[loser.len() / 2];
+    }
+
+    println!("{part2}");
 }
