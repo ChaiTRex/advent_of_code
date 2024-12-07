f7ee6303854d25a793d2dd7c8a7bdf22fee90c8a
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-07 00:13:31 -0500

    Initial solution to 2024 day 7 (currently only gives part 2)

--- 2024/rust/day07/src/main.rs
+++ 2024/rust/day07/src/main.rs
@@ -1,4 +1,34 @@
 fn main() {
     static INPUT: &str = include_str!("../../../day07.txt");
-    println!("{INPUT}");
+
+    fn f(correct_result: u64, operands: &[u64], current_result: u64) -> bool {
+        if operands.is_empty() {
+            return correct_result == current_result;
+        }
+        f(correct_result, &operands[1..], current_result + operands[0])
+            || f(correct_result, &operands[1..], current_result * operands[0])
+            || f(
+                correct_result,
+                &operands[1..],
+                (current_result.to_string() + &operands[0].to_string())
+                    .parse::<u64>()
+                    .unwrap(),
+            )
+    }
+
+    let mut part1 = 0;
+
+    for line in INPUT.lines() {
+        let (result, operands) = line.split_once(": ").unwrap();
+        let correct_result = result.parse::<u64>().unwrap();
+        let mut operands = operands
+            .split(' ')
+            .map(|operand| operand.parse::<u64>().unwrap())
+            .collect::<Vec<_>>();
+        if f(correct_result, &operands[1..], operands[0]) {
+            part1 += correct_result;
+        }
+    }
+
+    println!("{part1}");
 }
