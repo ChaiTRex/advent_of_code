450bb7a1cac792a2e2e6b3e45951559cfcab8cd7
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-07 00:27:01 -0500

    Now outputs both parts, and added timing to 2024 day 7

--- 2024/rust/day07/src/main.rs
+++ 2024/rust/day07/src/main.rs
@@ -7,7 +7,15 @@ fn main() {
         }
         f(correct_result, &operands[1..], current_result + operands[0])
             || f(correct_result, &operands[1..], current_result * operands[0])
-            || f(
+    }
+
+    fn g(correct_result: u64, operands: &[u64], current_result: u64) -> bool {
+        if operands.is_empty() {
+            return correct_result == current_result;
+        }
+        g(correct_result, &operands[1..], current_result + operands[0])
+            || g(correct_result, &operands[1..], current_result * operands[0])
+            || g(
                 correct_result,
                 &operands[1..],
                 (current_result.to_string() + &operands[0].to_string())
@@ -16,19 +24,26 @@ fn main() {
             )
     }
 
+    let start = std::time::Instant::now();
+
     let mut part1 = 0;
+    let mut part2 = 0;
 
     for line in INPUT.lines() {
         let (result, operands) = line.split_once(": ").unwrap();
         let correct_result = result.parse::<u64>().unwrap();
-        let mut operands = operands
+        let operands = operands
             .split(' ')
             .map(|operand| operand.parse::<u64>().unwrap())
             .collect::<Vec<_>>();
         if f(correct_result, &operands[1..], operands[0]) {
             part1 += correct_result;
+            part2 += correct_result;
+        } else if g(correct_result, &operands[1..], operands[0]) {
+            part2 += correct_result;
         }
     }
 
-    println!("{part1}");
+    let time = start.elapsed();
+    println!("Part 1: {part1}\nPart 2: {part2}\nTime taken: {time:?}",);
 }
