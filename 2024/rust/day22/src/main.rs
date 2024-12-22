f5fc56c315529cbe005049a05e6f0f9e184007ca
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-22 13:03:41 -0500

    Sped up 2024 day 22

--- 2024/rust/day22/src/main.rs
+++ 2024/rust/day22/src/main.rs
@@ -1,54 +1,53 @@
-use std::collections::{HashMap, HashSet};
-
 fn main() {
     static INPUT: &str = include_str!("../../../day22.txt");
 
+    let start = std::time::Instant::now();
+
+    let inputs = INPUT
+        .lines()
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
     let mut part1 = 0;
-    for mut n in INPUT.lines().map(|n| n.parse::<u32>().unwrap()) {
-        //let orig = n;
+    for mut n in inputs.iter().copied() {
         for _ in 0..2000 {
             n = next_secret(n);
         }
         part1 += n as u64;
-        //println!("{orig}: {n}");
     }
-    println!("{part1}");
 
-    let mut all_sequences = HashSet::new();
-    let mut all_sequence_prices = Vec::new();
-    for mut n in INPUT.lines().map(|n| n.parse::<u32>().unwrap()) {
-        let mut sequence_prices = HashMap::new();
+    let mut all_sequence_prices = [0; 126892];
+    for mut n in inputs.into_iter() {
+        let mut seen_changes = [false; 126892];
 
-        let mut units = Vec::with_capacity(2000);
-        for _ in 0..2001 {
-            units.push(n as i32 % 10);
+        let mut units = [0; 2001];
+        for i in 0..2001 {
+            units[i] = (n % 10) as i8;
             n = next_secret(n);
         }
         for xs in units.windows(5) {
-            let changes = [xs[1] - xs[0], xs[2] - xs[1], xs[3] - xs[2], xs[4] - xs[3]];
-            all_sequences.insert(changes);
-            sequence_prices.entry(changes).or_insert(xs[4]);
+            let changes = 65160
+                + xs[4] as usize
+                + 18 * xs[3] as usize
+                + 342 * xs[2] as usize
+                + 6498 * xs[1] as usize
+                - 6859 * xs[0] as usize;
+            if !seen_changes[changes] {
+                seen_changes[changes] = true;
+                all_sequence_prices[changes] += xs[4] as u16;
+            }
         }
-
-        all_sequence_prices.push(sequence_prices);
     }
-    let part2 = all_sequences
-        .into_iter()
-        .map(|sequence| {
-            all_sequence_prices
-                .iter()
-                .flat_map(|sequence_prices| sequence_prices.get(&sequence).map(|&n| n as i64))
-                .sum::<i64>()
-        })
-        .max()
-        .unwrap();
+    let part2 = all_sequence_prices.into_iter().max().unwrap();
 
-    println!("{part2}");
+    let time = start.elapsed();
+    println!("Part 1: {part1}\nPart 2: {part2}\nTime taken: {time:?}",);
 }
 
 fn next_secret(mut n: u32) -> u32 {
-    n = (n ^ (n << 6)) & 0b1111_1111_1111_1111_1111_1111;
-    n = (n ^ (n >> 5)) & 0b1111_1111_1111_1111_1111_1111;
-    n = (n ^ (n << 11)) & 0b1111_1111_1111_1111_1111_1111;
-    n
+    n ^= n << 6;
+    n &= 0b1111_1111_1111_1111_1111_1111;
+    n ^= n >> 5;
+    n ^= n << 11;
+    n & 0b1111_1111_1111_1111_1111_1111
 }
