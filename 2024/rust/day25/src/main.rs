e85c136dd8a20ee657f228824ea1a5ce2463276b
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2024-12-25 01:30:10 -0500

    Initial solution to 2024 day 25

--- 2024/rust/day25/src/main.rs
+++ 2024/rust/day25/src/main.rs
@@ -1,4 +1,47 @@
 fn main() {
     static INPUT: &str = include_str!("../../../day25.txt");
-    println!("{INPUT}");
+
+    let mut locks = Vec::new();
+    let mut keys = Vec::new();
+    for entry in INPUT.split("\n\n") {
+        let mut lines = entry.lines();
+        if lines.next().unwrap() == "#####" {
+            let mut heights = [0, 0, 0, 0, 0];
+            for (height, line) in (1..=5).zip(lines) {
+                for (i, ch) in (0..5).zip(line.bytes()) {
+                    if ch == b'#' {
+                        heights[i] = height;
+                    }
+                }
+            }
+            locks.push(heights);
+        } else {
+            let mut heights = [0, 0, 0, 0, 0];
+            for (height, line) in (1..=5).zip(lines.rev().skip(1)) {
+                for (i, ch) in (0..5).zip(line.bytes()) {
+                    if ch == b'#' {
+                        heights[i] = height;
+                    }
+                }
+            }
+            keys.push(heights);
+        }
+    }
+
+    let mut part1 = 0;
+    for lock in &locks {
+        for key in &keys {
+            if lock
+                .into_iter()
+                .copied()
+                .zip(key.into_iter().copied())
+                .map(|(lock_height, key_height)| lock_height + key_height)
+                .all(|height| height <= 5)
+            {
+                part1 += 1;
+            }
+        }
+    }
+
+    println!("{part1}");
 }
